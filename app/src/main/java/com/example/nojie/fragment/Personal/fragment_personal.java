6b957877a7303be0b8bd5f284f332088bc8982b3
banqingyuan <15606160125@163.com>
2019-11-16 23:52:33 +0800

    1.修复了fragment_square界面ScrollView与RecycleView界面嵌套导致的错误。 2.开始写personal界面布局，已完成上半部分

--- app/src/main/java/com/example/nojie/fragment/Personal/fragment_personal.java
+++ app/src/main/java/com/example/nojie/fragment/Personal/fragment_personal.java
@@ -1,9 +1,21 @@
 package com.example.nojie.fragment.Personal;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +29,9 @@ public class fragment_personal extends Fragment {
 
     private View root;
     private TitleBar titleBar;
+    private ImageView icon;
+    private TextView username;
+    private TextView usersign;
 
     @Nullable
     @Override
@@ -29,8 +44,19 @@ public class fragment_personal extends Fragment {
     }
     private void initContentView(View root) {
         titleBar = root.findViewById(R.id.personal_titleBar);
+        icon = root.findViewById(R.id.person_icon);
+        username = root.findViewById(R.id.personal_user_name);
+        usersign = root.findViewById(R.id.personal_user_sign);
     }
     private void initData() {
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.icon);
+        //设置bitmap.getWidth()可以获得圆形
+        Bitmap bitmap1 = ClipSquareBitmap(bitmap,200,bitmap.getWidth());
+        icon.setImageBitmap(bitmap1);
+        username.setText("单纯不做作的我");
+        usersign.setText("国家一级赖床运动员");
+
             titleBar.setLeftIcon(R.drawable.message);
             titleBar.setRightIcon(R.drawable.set_up);
             titleBar.setOnTitleBarListener(new OnTitleBarListener() {
@@ -50,6 +76,58 @@ public class fragment_personal extends Fragment {
                 }
             });
     }
+    //将ImageView中的图片传入并输出圆形bitmap
+    public static Bitmap ClipSquareBitmap(Bitmap bmp, int width, int radius) {
+        if (bmp == null || width <= 0)
+            return null;
+        //如果图片比较小就没必要进行缩放了
 
+        /**
+         * 把图片进行缩放，以宽高最小的一边为准，缩放图片比例
+         * */
+        if (bmp.getWidth() > width && bmp.getHeight() > width) {
+            if (bmp.getWidth() > bmp.getHeight()) {
+                bmp = Bitmap.createScaledBitmap(bmp, (int) (((float) width) * bmp.getWidth() / bmp.getHeight()), width, false);
+            } else {
+                bmp = Bitmap.createScaledBitmap(bmp, width, (int) (((float) width) * bmp.getHeight() / bmp.getWidth()), false);
+            }
+
+        } else {
+            width = bmp.getWidth() > bmp.getHeight() ? bmp.getHeight() : bmp.getWidth();
+            Log.d("zeyu","宽" + width + ",w" + bmp.getWidth() + ",h" + bmp.getHeight());
+            if (radius > width) {
+                radius = width;
+            }
+        }
+        Bitmap output = Bitmap.createBitmap(width, width, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        //设置画笔全透明
+        canvas.drawARGB(0, 0, 0, 0);
+        Paint paints = new Paint();
+        paints.setColor(Color.WHITE);
+        paints.setAntiAlias(true);//去锯齿
+        paints.setFilterBitmap(true);
+        //防抖动
+        paints.setDither(true);
+
+        //把图片圆形绘制矩形
+        if (radius <= 0)
+            canvas.drawRect(new Rect(0, 0, width, width), paints);
+        else //绘制圆角
+            canvas.drawRoundRect(new RectF(0, 0, width, width), radius, radius, paints);
+        // 取两层绘制交集。显示前景色。
+        paints.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        Rect rect = new Rect();
+        if (bmp.getWidth() >= bmp.getHeight()) {
+            rect.set((bmp.getWidth() - width) / 2, 0, (bmp.getWidth() + width) / 2, width);
+        } else {
+            rect.set(0, (bmp.getHeight() - width) / 2, width, (bmp.getHeight() + width) / 2);
+        }
+        Rect rect2 = new Rect(0, 0, width, width);
+        //第一个rect 针对bmp的绘制区域，rect2表示绘制到上面位置
+        canvas.drawBitmap(bmp, rect, rect2, paints);
+        bmp.recycle();
+        return output;
+    }
 
 }
